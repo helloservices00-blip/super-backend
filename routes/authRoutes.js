4f2500ecce86b5744a09a8f559faa0d9a6ab1d0c
helloservices00-blip <helloservices00@gmail.com>
2025-11-23 13:51:19 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,51 +1,9 @@
-const express = require("express");
+const express = require('express');
+const { signup, login } = require('../controllers/authController');
+
 const router = express.Router();
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-// USER SIGNUP
-router.post("/signup", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    let exists = await User.findOne({ email });
-    if (exists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashed = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashed,
-    });
-
-    res.json({ message: "Signup successful", user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// USER LOGIN
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    let user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "User not found" });
-
-    let isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ message: "Invalid password" });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
-    res.json({ message: "Login successful", token, user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
