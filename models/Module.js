d28f4b2eb2584ff5eb6b144cc0763bb385320974
helloservices00-blip <helloservices00@gmail.com>
2025-11-23 23:44:15 +0530

    Create Module.js

---
+++ models/Module.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const moduleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+}, { timestamps: true });
+
+const Module = mongoose.model("Module", moduleSchema);
+export default Module;
