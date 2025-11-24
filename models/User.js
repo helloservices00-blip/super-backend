8881d96d04e97fd87d970c475ef89e7ba7569b9b
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 21:52:52 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,31 @@
+// models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin", "vendor"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
