83d5502d891ae3a9bad3349c0cde1272e1c72387
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 10:30:45 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const { body } = require('express-validator');
-const { createProduct, getProducts, updateProduct, deleteProduct } = require('../controllers/productController');
-const { protect } = require('../middlewares/authMiddleware');
+const router = require("express").Router();
+const { getProductsBySubcategory, createProduct } = require("../controllers/productController");
 
-router.post('/', protect, [
-  body('name').notEmpty(),
-  body('price').isNumeric()
-], createProduct);
-
-router.get('/', getProducts);
-router.put('/:id', protect, updateProduct);
-router.delete('/:id', protect, deleteProduct);
+// relative to app.js /api/subcategories
+router.get("/:subcategoryId/products", getProductsBySubcategory);
+router.post("/", createProduct);
 
 module.exports = router;
