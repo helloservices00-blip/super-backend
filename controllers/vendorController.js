68ccb50a02a37cc0b16801f7077b5c228846c802
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 10:36:23 +0530

    Update vendorController.js

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,32 +1,24 @@
-const Vendor = require('../models/Vendor');
-const jwt = require('jsonwebtoken');
-const { validationResult } = require('express-validator');
+const Vendor = require("../models/Vendor");
 
-const generateToken = (id, role) => jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
-
-const registerVendor = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-  const { name, email, password, shopName } = req.body;
-  const exists = await Vendor.findOne({ email });
-  if (exists) return res.status(400).json({ message: 'Vendor exists' });
-  const vendor = await Vendor.create({ name, email, password, shopName });
-  res.status(201).json({ _id: vendor._id, name: vendor.name, email: vendor.email, shopName: vendor.shopName, role: vendor.role, token: generateToken(vendor._id, vendor.role) });
-};
-
-const loginVendor = async (req, res) => {
-  const { email, password } = req.body;
-  const vendor = await Vendor.findOne({ email });
-  if (vendor && (await vendor.matchPassword(password))) {
-    res.json({ _id: vendor._id, name: vendor.name, email: vendor.email, shopName: vendor.shopName, role: vendor.role, token: generateToken(vendor._id, vendor.role) });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
+// Get all vendors of a specific module
+exports.getVendorsByModule = async (req, res) => {
+  try {
+    const { moduleId } = req.params;
+    const vendors = await Vendor.find({ module: moduleId });
+    res.json(vendors);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
 
-const listVendors = async (req, res) => {
-  const vendors = await Vendor.find().select('-password');
-  res.json(vendors);
+// Create a new vendor under a module
+exports.createVendor = async (req, res) => {
+  try {
+    const { name, module } = req.body;
+    const vendor = new Vendor({ name, module });
+    await vendor.save();
+    res.status(201).json(vendor);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
-
-module.exports = { registerVendor, loginVendor, listVendors };
